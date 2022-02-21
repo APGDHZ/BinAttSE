e5a0c62e291ae5bc309cb73e6022f2b8690564c9
Tom Gajecki <tomgajecki@gmail.com>
2022-02-21 21:08:58 +0100

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -14,13 +14,12 @@ import librosa
 from tqdm import tqdm
 import tensorflow as tf
 
-
-class DataGenerator:
+class DataGenerator():
     def __init__(self, mode, args):
-        if mode != "train" and mode != "valid" and mode != "test" and mode != "objective_test":
+        if mode != "train" and mode != "valid" and mode != "test":
             raise ValueError("mode: {} while mode should be "
                              "'train', or 'test'".format(mode))
-
+        
         if not os.path.isdir(args.data_dir):
             raise ValueError("cannot find data_dir: {}".format(args.data_dir))
 
@@ -36,113 +35,117 @@ class DataGenerator:
 
     def _float_list_feature(self, value):
         return tf.train.Feature(float_list=tf.train.FloatList(value=value))
-
+            
     def fetch(self):
         dataset = tf.data.TFRecordDataset(self.tfr).map(self._decode,
                                                         num_parallel_calls=tf.data.experimental.AUTOTUNE)
         if self.mode == "train":
-            tf.random.set_seed(42)
-            dataset = dataset.shuffle(2000, reshuffle_each_iteration=True, seed=42)
+            dataset = dataset.shuffle(2000, reshuffle_each_iteration=True)
             train_dataset = dataset.batch(self.batch_size, drop_remainder=True)
             train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
             return train_dataset
-
+        
         if self.mode == "valid":
             valid_dataset = dataset.batch(1, drop_remainder=True)
-            valid_dataset = valid_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+            valid_dataset = valid_dataset.prefetch(tf.data.experimental.AUTOTUNE) 
             return valid_dataset
-
+        
         else:
             dataset = dataset.batch(1, drop_remainder=True)
             dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
             return dataset
 
     def _encode(self, mode):
-
+        
         if self.mode == "train":
             print("\nSerializing training data...\n")
-
+        
         if self.mode == "valid":
             print("\nSerializing validation data...\n")
-
+        
         if self.mode == "test":
             print("\nSerializing testing data...\n")
-
-        if self.mode == "objective_test":
-            print("\nSerializing testing data for objective evaluation...\n")
-
+            
         writer = tf.io.TFRecordWriter(self.tfr)
-
-        if self.mode != "objective_test":
-            mix_filenames = glob.glob(os.path.join(self.wav_dir, "*mixed.wav"))
+        
+        if self.mode != "test":
+            mix_filenames = glob.glob(os.path.join(self.wav_dir, "*mixure.wav"))
             target_filenames = glob.glob(os.path.join(self.wav_dir, "*target.wav"))
-            sys.stdout.flush()
-
-            for mix_filename, target_filename in tqdm(zip(mix_filenames,
-                                                          target_filenames), total=len(mix_filenames)):
-                mix, _ = librosa.load(mix_filename, sr=self.sample_rate, mono=False)
-                clean, _ = librosa.load(target_filename, sr=self.sample_rate, mono=False)
-
+            sys.stdout.flush()  
+            
+            for mix_filename, target_filename in tqdm(zip(mix_filenames, 
+                                                          target_filenames), total = len(mix_filenames)):
+                mix, _ = librosa.load(mix_filename, self.sample_rate, mono = False)
+                clean, _ = librosa.load(target_filename, self.sample_rate, mono = False)
+    
                 def write(a, b):
                     example = tf.train.Example(
                         features=tf.train.Features(
                             feature={
-                                "noisy_left": self._float_list_feature(mix[0, a:b]),
+                                "noisy_left" : self._float_list_feature(mix[0, a:b]),
                                 "noisy_right": self._float_list_feature(mix[1, a:b]),
-                                "clean_left": self._float_list_feature(clean[0, a:b]),
+                                "clean_left" : self._float_list_feature(clean[0, a:b]),
                                 "clean_right": self._float_list_feature(clean[1, a:b])}))
-
+                    
                     writer.write(example.SerializeToString())
-
+                
                 now_length = mix.shape[-1]
                 target_length = int(self.duration * self.sample_rate)
-
+    
                 if now_length < target_length:
-                    continue
-
+                    continue 
+                
                 stride = int(self.duration * self.sample_rate)
                 for i in range(0, now_length - target_length, stride):
                     write(i, i + target_length)
         else:
-            mix_filenames = glob.glob(os.path.join(self.wav_dir, "*mixed.wav"))
-            target_filenames = glob.glob(os.path.join(self.wav_dir, "*target.wav"))
-            sys.stdout.flush()
-
-            for mix_filename, target_filename in tqdm(zip(mix_filenames,
-                                                          target_filenames), total=len(mix_filenames)):
-                mix, _ = librosa.load(mix_filename, sr=self.sample_rate, mono=False)
-                clean, _ = librosa.load(target_filename, sr=self.sample_rate, mono=False)
-
+            mix_filenames = glob.glob(os.path.join(self.wav_dir, "*mixure.wav"))
+            sys.stdout.flush()  
+            
+            for mix_filename in tqdm(mix_filenames, total = len(mix_filenames)):
+                mix, _ = librosa.load(mix_filename, self.sample_rate, mono = False)
+    
                 def write(a, b):
                     example = tf.train.Example(
                         features=tf.train.Features(
                             feature={
-                                "noisy_left": self._float_list_feature(mix[0, a:b]),
-                                "noisy_right": self._float_list_feature(mix[1, a:b]),
-                                "clean_left": self._float_list_feature(clean[0, a:b]),
-                                "clean_right": self._float_list_feature(clean[1, a:b])}))
-
+                                "noisy_left" : self._float_list_feature(mix[0, a:b]),
+                                "noisy_right": self._float_list_feature(mix[1, a:b])}))
+                    
                     writer.write(example.SerializeToString())
-
+                
                 write(None, None)
         writer.close()
         sys.stdout.flush()
-
+              
     def _decode(self, serialized_example):
-        example = tf.io.parse_single_example(
-            serialized_example,
-            features={
-                "noisy_left": tf.io.VarLenFeature(tf.float32),
-                "noisy_right": tf.io.VarLenFeature(tf.float32),
-                "clean_left": tf.io.VarLenFeature(tf.float32),
-                "clean_right": tf.io.VarLenFeature(tf.float32)})
-
-        noisy_left = tf.sparse.to_dense(example["noisy_left"])
-
-        noisy_right = tf.sparse.to_dense(example["noisy_right"])
-
-        clean_left = tf.sparse.to_dense(example["clean_left"])
-
-        clean_right = tf.sparse.to_dense(example["clean_right"])
-
-        return (noisy_left, noisy_right), (clean_left, clean_right)
+        if self.mode != "test":
+            example = tf.io.parse_single_example(
+                serialized_example,
+                features={
+                    "noisy_left":  tf.io.VarLenFeature(tf.float32),
+                    "noisy_right": tf.io.VarLenFeature(tf.float32),
+                    "clean_left":  tf.io.VarLenFeature(tf.float32),
+                    "clean_right": tf.io.VarLenFeature(tf.float32)})
+            
+            noisy_left =  tf.sparse.to_dense(example["noisy_left"])
+     
+            noisy_right = tf.sparse.to_dense(example["noisy_right"])
+            
+            clean_left =  tf.sparse.to_dense(example["clean_left"])
+            
+            clean_right = tf.sparse.to_dense(example["clean_right"])
+            
+            return (noisy_left, noisy_right), (clean_left, clean_right)
+        else:
+            example = tf.io.parse_single_example(
+                serialized_example,
+                features={
+                    "noisy_left": tf.io.VarLenFeature(tf.float32),
+                    "noisy_right": tf.io.VarLenFeature(tf.float32)})
+            
+            noisy_left =  tf.sparse.to_dense(example["noisy_left"])
+     
+            noisy_right = tf.sparse.to_dense(example["noisy_right"])
+            
+            return noisy_left, noisy_right
